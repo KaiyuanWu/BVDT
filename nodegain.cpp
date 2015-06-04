a77b09b3371df8285f77735a64fe9e0d73d84a19
Wu Kaiyuan <kaiwu@buaa.edu.cn>
2015-06-04 16:01:07 +0800

    rearragement

--- nodegain.cpp
+++
@@ -1,6 +0,0 @@
-#include "nodegain.h"
-
-NodeGain::NodeGain(SolverParameter::RegularizationType regularization_type): _regularization_type(regularization_type)
-{
-
-}
--- nodemode.cpp
+++
@@ -1,4 +0,0 @@
-#include "nodemode.h"
-NodeMode::NodeMode(int nclass, bvdt::__NodeMode_ __node_mode_, float *param): _nclass(nclass), ___node_mode_(__node_mode_)
-{
-}
